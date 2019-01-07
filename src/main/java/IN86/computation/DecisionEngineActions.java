e223ff88a363262e5ffa9519fd8ca7d3b0b10a7c
Nikhil Ninawe <nikhil.n@turvo.com>
2019-01-07 15:47:05 +0530

    quarantine issue fix

--- src/main/java/IN86/computation/DecisionEngineActions.java
+++ src/main/java/IN86/computation/DecisionEngineActions.java
@@ -8,7 +8,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Objects;
 
 @Component
 public class DecisionEngineActions {
@@ -25,13 +27,28 @@ public class DecisionEngineActions {
 
     public void quarantine(String instance, double score){
         slackApi.call(new SlackMessage("Quarantining instance " + instance + ". Its score is " + score));
+        ClassLoader classLoader = DecisionEngineActions.class.getClassLoader();
+        String fileName = "quarantine.sh";
+        File file = new File(classLoader.getResource(fileName).getFile());
 
-        String[] cmd = {"resources/quarantine.sh", instance};
+        String[] cmd = {file.getAbsolutePath(), instance};
+        Process p = null;
         try {
-            Process p = Runtime.getRuntime().exec(cmd);
-        } catch (IOException e) {
+            p = Runtime.getRuntime().exec(cmd);
+            Thread.sleep(20*1000);
+            logger.info("Quarantined instance " + instance + " successfully.");
+            slackApi.call(new SlackMessage("Quarantined instance " + instance + " successfully."));
+        } catch (Exception e) {
             logger.error("Error in Quarantining instance " + instance + ".", e);
-            slackApi.call(new SlackMessage("Error in Quarantining instance " + instance + "."));git add
+            slackApi.call(new SlackMessage("Error in Quarantining instance " + instance + "."));
         }
+        if(Objects.nonNull(p)) {
+            logger.info("Exit value" + p.exitValue());
+        }
+    }
+
+    public static void main(String[] args){
+        DecisionEngineActions actions = new DecisionEngineActions();
+        actions.quarantine("I1", 1.2);
     }
 }
